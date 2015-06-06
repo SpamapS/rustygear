f1de1292d00969c11b998cb2aa03c9534f43df44
Clint Byrum <clint@fewbar.com>
2015-06-06 16:30:47 -0700

    More code ported from python gear

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 extern crate time;
+extern crate byteorder;
 
 use std::option::Option;
 use std::cmp::Ordering;
+use std::io::Write;
+use std::net::TcpStream;
+
+use byteorder::{BigEndian, WriteBytesExt};
 
 #[test]
 fn constructor() {
@@ -36,18 +41,27 @@ enum ConnectionState {
 
 struct Connection {
     host: String,
-    port: u32,
+    port: u16,
+    conn: Option<TcpStream>,
     connected: bool,
     connect_time: Option<time::Tm>,
     state: ConnectionState,
     state_time: Option<time::Tm>,
 }
 
+struct Packet<'a> {
+    code: u32,
+    ptype: u32,
+    data: &'a [u8],
+    conn: Option<&'a Connection>,
+}
+
 impl Connection {
-    fn new(host: &str, port: u32) -> Connection {
+    fn new(host: &str, port: u16) -> Connection {
         let mut c = Connection {
             host: host.to_string(),
             port: port,
+            conn: None,
             connected: false,
             connect_time: None,
             state: ConnectionState::INIT,
@@ -60,5 +74,60 @@ impl Connection {
     fn change_state(&mut self, state: ConnectionState) {
         self.state_time = Some(time::now_utc());
         self.state = state;
+    }
+
+    fn connect(&mut self) {
+        match self.conn {
+            None => {
+                self.conn = Some(TcpStream::connect((&*self.host, self.port)).unwrap());
+                self.connected = true;
+                self.connect_time = Some(time::now_utc());
+            },
+            Some(_) => { /* log debug "already connected" */ },
+        }
+    }
+
+    fn disconnect(&mut self) {
+        match self.conn {
+            Some(_) => self.conn = None,
+            None => { /* log debug "already disconnected" */ },
+        }
+    }
+
+    fn reconnect(&mut self) {
+        self.disconnect();
+        self.connect()
+    }
+
+    fn sendPacket(&mut self, packet: &Packet) {
+        match self.conn {
+            Some(ref mut c) => {
+                c.write_u32::<BigEndian>(packet.code).unwrap();
+                c.write_u32::<BigEndian>(packet.ptype);
+                c.write_u32::<BigEndian>(packet.data.len() as u32).unwrap();
+                c.write_all(packet.data).unwrap()
+            },
+            None => panic!("Attempted to send packet on disconnected socket")
+        }
+    }
+}
+
+impl<'a> PartialEq for Packet<'a> {
+    fn eq(&self, other: &Packet) -> bool {
+        self.code == other.code &&
+        self.ptype == other.ptype &&
+        self.data == other.data
+    }
+}
+
+
+impl<'a> Packet<'a> {
+    fn new(code: u32, ptype: u32, data: &'a [u8]) -> Packet {
+        Packet {
+            code: code,
+            ptype: ptype,
+            data: data,
+            conn: None,
+        }
     }
 }
