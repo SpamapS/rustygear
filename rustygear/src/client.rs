92883b6a5134e3bb8ed9fa13ca82c5264ad15fb3
Clint Byrum <clint@fewbar.com>
2018-12-16 18:41:06 -0800

    use hash_ring

--- rustygear/src/client.rs
+++ rustygear/src/client.rs
@@ -1,59 +1,70 @@
 use std::hash::{Hash, Hasher};
-use std::collections::HashSet;
+extern crate hash_ring;
+use client::hash_ring::HashRing;
+use std::fmt;
 
 #[test]
 fn test_basic_client() {
     let mut c = Client::new();
-    c.add_server("::1");
+    c.add_server("::1", None);
     //let response = c.echo("foo");
     //assert_eq!(response, "foo");
-    c.remove_server("::1");
+    c.remove_server("::1", None);
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Clone)]
 enum ConnectionState {
     INIT,
     CONNECTING,
     CONNECTED,
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Clone)]
 struct Server {
-    hostname: String,
+    host: String,
+    port: u16,
     state: ConnectionState,
 }
 
 impl Server {
-    fn new(hostname: &String) -> Server {
+    fn new(host: &String, port: Option<u16>) -> Server {
         Server {
-            hostname: hostname.clone(),
+            host: host.clone(),
+            port: port.unwrap_or(11211),
             state: ConnectionState::INIT,
         }
     }
 }
 
+impl fmt::Display for Server {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}", &self.host, self.port)
+    }
+}
+
 impl Hash for Server {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        self.hostname.hash(state);
+        self.host.hash(state);
+        self.port.hash(state);
     }
 }
 
 struct Client {
-    servers: HashSet<Server>,
+    servers: HashRing<Server>,
 }
 
 impl Client {
     fn new() -> Client {
         Client {
-            servers: HashSet::new(),
+            servers: HashRing::new(Vec::new(), 1),
         }
     }
 
-    fn add_server<S: Into<String>>(&mut self, hostname: S) {
-       self.servers.insert(Server::new(&hostname.into()));
+    fn add_server<S: Into<String>>(&mut self, host: S, port: Option<u16>) {
+       self.servers.add_node(&Server::new(&host.into(), port));
     }
 
-    fn remove_server<S: Into<String>>(&mut self, hostname: S) {
-       self.servers.remove(&Server::new(&hostname.into()));
+    fn remove_server<S: Into<String>>(&mut self, host: S, port: Option<u16>) {
+       self.servers.remove_node(&Server::new(&host.into(), port));
     }
 }
