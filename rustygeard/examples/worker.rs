626fccd436d62f12303bc5621ee160687958ceee
Clint Byrum <clint@fewbar.com>
2020-04-10 09:19:25 -0700

    Fix up example worker
    Before was just a guess, but as the API has built around client, it's
    become more clear this is how worker will be implemented. Also fleshed
    it out with more types of things.

--- rustygeard/examples/worker.rs
+++ rustygeard/examples/worker.rs
@@ -1,11 +1,28 @@
+use std::time::Duration;
+use std::thread;
+
 use tokio::prelude::*;
+
+use rustygeard::client::{Worker, WorkerJob};
+
+/// When we use the status method, we need to be async!
+async fn status_user(job: WorkerJob) -> Result<(), io::Error> {
+    job.status(50, 100).await;
+    thread::sleep(Duration::from_secs(1));
+    Ok("all done")
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let worker = Worker::new().add_host('::1');
-    if let Ok(worker) = worker.connect().await {
-        worker.work(|job| {
-            job.response(job.data().reverse())
+    let mut worker = Worker::new();
+    worker
+        .connect()
+        .await?
+        .can_do("reverse", |job| Ok(job.data().reverse()))
+        .can_do("alwaysfail", |job| {
+            Err(io::Error::new(io::ErrorKind::Other, "Always fails"))
         })
-    }
+        .can_do_async("status", status_user)
+        .work()
+        .await?
 }
