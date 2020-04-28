0c7631f514dee4e63a09b1f050a1a06245fb989c
Clint Byrum <clint@fewbar.com>
2020-04-27 21:00:30 -0700

    Add command line flag for listening
    Also adding the new base level target dir to .gitignore

--- rustygeard/src/main.rs
+++ rustygeard/src/main.rs
@@ -6,11 +6,26 @@ extern crate rustygear;
 extern crate rustygeard;
 
 use rustygeard::server::GearmanServer;
+use clap::{Arg, App};
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
+    let matches = App::new("rustygeard")
+        .version(VERSION)
+        .about("See http://gearman.org/protocol")
+        .arg(Arg::with_name("listen")
+            .short("L")
+            .long("listen")
+            .value_name("Address:port")
+            .help("Server will listen on this address")
+            .takes_value(true))
+        .get_matches();
+
+    let listen = matches.value_of("listen").unwrap_or("0.0.0.0:4730");
     env_logger::init();
 
-    info!("Binding to 0.0.0.0:4730");
-    let address = "0.0.0.0:4730".parse().unwrap();
+    info!("Binding to {}", listen);
+    let address = listen.parse().unwrap();
     GearmanServer::run(address);
 }
