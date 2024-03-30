37c385ada6ed9691c427ecfce4af7d7aad98c9f4
Clint Byrum <clint@fewbar.com>
2024-03-30 15:55:43 -0700

    upgrading some incompatible deps

--- rustygeard/src/main.rs
+++ rustygeard/src/main.rs
@@ -5,24 +5,29 @@ extern crate env_logger;
 extern crate rustygear;
 extern crate rustygeard;
 
+use clap::{command, Arg};
 use rustygeard::server::GearmanServer;
-use clap::{Arg, App};
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    let matches = App::new("rustygeard")
+    let matches = command!()
         .version(VERSION)
         .about("See http://gearman.org/protocol")
-        .arg(Arg::with_name("listen")
-            .short("L")
-            .long("listen")
-            .value_name("Address:port")
-            .help("Server will listen on this address")
-            .takes_value(true))
+        .arg(
+            Arg::new("listen")
+                .short('L')
+                .long("listen")
+                .value_name("Address:port")
+                .help("Server will listen on this address")
+                .default_value("0.0.0.0:4730")
+                .num_args(1),
+        )
         .get_matches();
 
-    let listen = matches.value_of("listen").unwrap_or("0.0.0.0:4730");
+    let listen = matches
+        .get_one::<String>("listen")
+        .expect("default_value should ensure this");
     env_logger::init();
 
     info!("Binding to {}", listen);
