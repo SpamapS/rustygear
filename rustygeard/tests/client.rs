5fe6b940e0b11920a9a4ca0d2cbd5784a05f77c0
Clint Byrum <clint@fewbar.com>
2024-04-01 07:03:40 -0700

    testing uniq usage in workers

--- rustygeard/tests/client.rs
+++ rustygeard/tests/client.rs
@@ -12,6 +12,21 @@ async fn connect(addr: &SocketAddr) -> Client {
         .expect("Failed to connect to server")
 }
 
+async fn worker(addr: &SocketAddr) -> Client {
+    connect(addr)
+        .await
+        .can_do("testfunc", |workerjob| {
+            Ok(format!(
+                "worker saw {} with unique [{}]",
+                String::from_utf8_lossy(workerjob.payload()),
+                String::from_utf8_lossy(workerjob.unique())
+            )
+            .into_bytes())
+        })
+        .await
+        .expect("CAN_DO should work")
+}
+
 #[tokio::test]
 async fn test_client_connects() {
     let server = start_test_server().unwrap();
@@ -20,8 +35,6 @@ async fn test_client_connects() {
 
 #[tokio::test]
 async fn test_client_echo() {
-    // This is here as an example of how to use env_logger to watch parts of the test runex
-    //std::env::set_var("RUST_LOG", "rustygear::client=debug");
     let server = start_test_server().unwrap();
     let mut client = connect(server.addr()).await;
     client.echo(b"Hello World").await.expect("Echo Failed");
@@ -37,18 +50,7 @@ async fn test_client_submit() {
         .expect("Submit job should return a client job");
     assert!(job.handle().handle().len() > 0);
 
-    let mut worker = connect(server.addr())
-        .await
-        .can_do("testfunc", |workerjob| {
-            Ok(format!(
-                "worker saw {} with unique [{}]",
-                String::from_utf8_lossy(workerjob.payload()),
-                String::from_utf8_lossy(workerjob.unique())
-            )
-            .into_bytes())
-        })
-        .await
-        .expect("CAN_DO should work");
+    let mut worker = worker(server.addr()).await;
     worker
         .do_one_job()
         .await
@@ -80,10 +82,22 @@ async fn test_client_submit() {
 async fn test_client_submit_unique() {
     let server = start_test_server().unwrap();
     let mut client = connect(server.addr()).await;
-    let ujob = client
-        .submit_unique("testunique", b"id:12345", b"bbbbb")
+    let mut ujob = client
+        .submit_unique("testfunc", b"id:12345", b"bbbbb")
         .await
         .expect("Submit unique should return a client job");
     assert!(ujob.handle().handle().len() > 0);
-    // TODO: GRAB_JOB_UNIQ is not implemented yet
+    let mut worker = worker(server.addr()).await;
+    worker
+        .do_one_job()
+        .await
+        .expect("Doing the job should work");
+    let response = ujob.response().await.expect("need response from worker");
+    if let WorkUpdate::Complete { handle, payload } = response {
+        assert_eq!(&handle, ujob.handle().handle());
+        let response_payload = String::from_utf8_lossy(&payload);
+        assert_eq!(&response_payload, "worker saw bbbbb with unique [id:12345]");
+    } else {
+        panic!("Worker did not send WORK_COMPLETE")
+    }
 }
